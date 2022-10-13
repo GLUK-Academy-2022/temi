6b51a31e169a5669cb314da4adf2e1ec6f46ae26
Temi Alao <temi.alao@ecs.co.uk>
2022-10-13 12:39:34 +0100

    do readme

---
+++ README.md
@@ -0,0 +1,9 @@
+python flask api
+
+install python3
+
+install pip3
+
+intall flask
+
+run python3 main.py or flask --app main run
--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-from flask import *
-import json, time
+from flask import Flask
 
 app = Flask(__name__)
-
-
-@app.route("/", methods=["GET"])
-def home_page():
-    data_set = {
-        "Page": "Home",
-        "Message": "Successfully loaded home page",
-        "Timestamp": time.time(),
-    }
-    json_dump = json.dumps(data_set)
-
-    return json_dump
 
 
 @app.route("/hello", methods=["GET"])
 def hello():
 
-    return "teamname"
+    return "Team Name"
 
 
 if __name__ == "__main__":
